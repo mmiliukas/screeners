a3f218f0f58af8db7ea434bd9b444b17e951a270
mmiliukas <mmiliukas@gmail.com>
2023-12-13 23:29:21 +0200

    .

---
+++ yahoo-correlation.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import yfinance as yf
+
+def adjust(date, days: int):
+    adjusted = date + timedelta(days=days)
+    adjusted = date if adjusted >= date else adjusted
+
+    return adjusted.isoformat()
+
+if __name__ == '__main__':
+    sectors = pd.read_csv('./sector-etf.csv')
+    daily_runs = pd.read_csv('./all.csv', parse_dates=['Date'])
+    tickers = pd.read_json('./all-tickers.json')
+
+    result = pd.merge(daily_runs, tickers, left_on="Symbol",right_on="symbol")
+    result = result[['Symbol', 'Name', 'Date', 'sector', 'industry', 'country']].sort_values(by='sector')
+    result = pd.merge(result, sectors, left_on="sector", right_on="Sector")
+    result = result[['Symbol', 'Name', 'Date', 'sector', 'industry', 'country', 'EU', 'US']]
+
+    print(result.head())
+    
+    ticker = yf.Ticker(result['Symbol'][0])
+    us = yf.Ticker(result['US'][0].split(sep=',')[0])
+    date = result['Date'][0].date()
+
+    a = ticker.history(start=adjust(date, -84), end=adjust(date, 14))
+    b = us.history(start=adjust(date, -84), end=adjust(date, 14))
+
+    fig, ax = plt.subplots()
+    a.plot(y='Close', ax=ax)
+    b.plot(y='Close', ax=ax, secondary_y=True)
+
+    plt.show()
