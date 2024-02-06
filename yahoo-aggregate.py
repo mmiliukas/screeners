5d14ffe41418ecf8ec4f70fd14dbf4a8ca80b6b5
mmiliukas <mmiliukas@gmail.com>
2024-02-06 21:06:12 +0200

    .

--- yahoo-aggregate.py
+++ yahoo-aggregate.py
@@ -10,3 +10,16 @@ if __name__ == '__main__':
     aggregate('./runs/*.csv', './all.csv')
     aggregate('./runs-loosers/*.csv', './all-loosers.csv')
     aggregate('./runs-random/*.csv', './all-random.csv')
+
+
+    winners = pd.read_csv('./all.csv')
+    winners['Screener'] = 'Winners'
+
+    loosers = pd.read_csv('./all-loosers.csv')
+    loosers['Screener'] = 'Loosers'
+
+    random = pd.read_csv('./all-random.csv')
+    random['Screener'] = 'Random'
+
+    all = pd.concat([winners, loosers, random], ignore_index=True)
+    all.to_csv('./all-combined.csv', index=False)
