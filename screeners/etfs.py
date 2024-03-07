1cc3b52e429ba29159b96654ca47d8d0a8cca21b
mmiliukas <mmiliukas@gmail.com>
2024-03-08 00:24:19 +0200

    .

---
+++ screeners/etfs.py
@@ -0,0 +1,13 @@
+SECTOR_ETF = {
+  'Basic Materials': 'XLB',
+  'Financial Services': 'XLF',
+  'Consumer Defensive': 'XLP',
+  'Utilities': 'XLU',
+  'Energy': 'XLE',
+  'Technology': 'XLK',
+  'Consumer Cyclical': 'XLY',
+  'Real Estate': 'XLRE',
+  'Healthcare': 'XLV',
+  'Communication Services': 'XLC',
+  'Industrials': 'XLI'
+}
