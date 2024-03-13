e40df4060baf0727b3038d651b31083b439c5b41
mmiliukas <mmiliukas@gmail.com>
2024-03-13 21:58:00 +0200

    .

--- yahoo-report.py
+++ yahoo-report.py
@@ -9,11 +9,14 @@ from screeners.telegram import log_to_telegram
 def main(argv):
     bot_token, channel_id = argv[1:]
 
-    tickers = len(pd.read_csv(config["tickers"]["target"]))
-    ignore = len(pd.read_csv(config["ignored_tickers"]["target"]))
+    tickers = pd.read_csv(config["tickers"]["target"])
+    ignored_tickers = pd.read_csv(config["ignored_tickers"]["target"])
 
-    message = f"<b>DAILY RUN:</b> <code>{tickers}</code> tickers matched and <code>{ignore}</code> ignored"
+    message = f"<b>DAILY RUN:</b> <code>{len(tickers)}</code> tickers matched and <code>{len(ignored_tickers)}</code> ignored"
     log_to_telegram(message, bot_token, channel_id)
+
+    ignored_summary = ignored_tickers.groupby("Reason").count().to_string()
+    log_to_telegram(f"<pre>{ignored_summary}</pre>", bot_token, channel_id)
 
 
 if __name__ == "__main__":
