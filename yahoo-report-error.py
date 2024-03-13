3aebdcab2e68429f7d2471c29b4449b431ac9866
mmiliukas <mmiliukas@gmail.com>
2024-03-13 22:34:10 +0200

    .

---
+++ yahoo-report-error.py
@@ -0,0 +1,14 @@
+import sys
+
+from screeners.telegram import log_to_telegram
+
+
+def main(argv):
+    bot_token, channel_id = argv[1:]
+
+    message = "<code>DAILY RUN FAILED!</code>"
+    log_to_telegram(message, bot_token, channel_id)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
