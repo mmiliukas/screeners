fb434508f464338a2d0889a172f4121023d5c489
Mantas Miliukas <mmiliukas@gmail.com>
2024-06-29 14:10:38 +0300

    chore(graph): add one more graph

--- yahoo-report-pages.py
+++ yahoo-report-pages.py
@@ -19,14 +19,48 @@ with open("config-logging.yml", "r") as config_logging:
 screener_names = [screener["name"] for screener in config["screeners"]]
 
 
-def tickers_frequency_bar(df: pd.DataFrame) -> Figure:
+def tickers_frequency(df: pd.DataFrame) -> Figure:
     grouped = df[screener_names].astype(bool).sum(axis=0).sort_values(ascending=False)
     grouped = grouped.to_frame("Count").reset_index(names="Screener")
 
-    title = "Tickers per screener (not unique)"
+    params = {
+        "data_frame": grouped,
+        "x": "Screener",
+        "y": "Count",
+        "text": "Count",
+        "height": 400,
+    }
 
-    fig = px.bar(grouped, x="Screener", y="Count", text="Count", title=title)
-    fig.update_traces(textposition="outside")
+    fig = px.bar(**params)
+    fig.update_xaxes(title="")
+    fig.update_yaxes(title="")
+    fig.update_traces(textposition="auto", showlegend=False)
+
+    return fig
+
+
+def tickers_frequency_group(df: pd.DataFrame) -> Figure:
+    grouped = df[screener_names].astype(bool).sum(axis=0).sort_values(ascending=False)
+    grouped = grouped.to_frame("Count").reset_index(names="Screener")
+    grouped["Group"] = grouped["Screener"].apply(lambda x: x.split(" ")[0])
+    grouped["Price Range"] = grouped["Screener"].apply(lambda x: x.split(" ")[1])
+    grouped["Price Range #"] = grouped["Screener"].apply(lambda x: int(x.split(" ")[1]))
+    grouped.sort_values(by=["Price Range #", "Group"], inplace=True)
+
+    params = {
+        "data_frame": grouped,
+        "x": "Price Range",
+        "y": "Count",
+        "color": "Group",
+        "text": "Group",
+        "height": 400,
+        "barmode": "group",
+    }
+
+    fig = px.bar(**params)
+    fig.update_xaxes(title=None)
+    fig.update_yaxes(title=None)
+    fig.update_traces(textposition="auto", showlegend=False)
 
     return fig
 
@@ -58,8 +92,8 @@ def write_html(figs: list[Figure], filename: str) -> None:
 def main(argv):
     tickers = read_tickers()[0]
 
-    fig = tickers_frequency_bar(tickers)
-    write_html([fig], "./pages/index.html")
+    figs = [tickers_frequency(tickers), tickers_frequency_group(tickers)]
+    write_html(figs, "./pages/index.html")
 
 
 if __name__ == "__main__":
