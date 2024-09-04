3e79fbe2e99eb0e476e5b1fbc89483383f18ce7d
Mantas Miliukas <mmiliukas@gmail.com>
2024-09-04 17:26:34 +0300

    chore(tickers): revive

--- yahoo-revive.py
+++ yahoo-revive.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python
 
+import logging
 from datetime import date, timedelta
 from typing import List
 
 import pandas as pd
 import yfinance as yf
+from tqdm import tqdm
+
+logging.getLogger("yfinance").setLevel(logging.CRITICAL)
 
 
 def revive(df: pd.DataFrame) -> List[str]:
     revived = []
 
-    for symbol in df["Symbol"].values:
-        ticker = yf.Ticker(symbol)
-        ticker_info = ticker.info
+    with tqdm(total=len(df)) as progress:
+        for symbol in df["Symbol"].values:
+            progress.set_description(symbol)
+            progress.update(1)
 
-        if not ticker_info:
-            # 404
-            continue
+            ticker = yf.Ticker(symbol)
+            ticker_info = ticker.info
 
-        if ticker_info.get("symbol") != symbol:
-            # 404
-            continue
+            if not ticker_info:
+                # 404
+                continue
 
-        if not ticker_info.get("sector"):
-            # not categorized
-            continue
+            if ticker_info.get("symbol") != symbol:
+                # 404
+                continue
 
-        current_ratio = ticker_info.get("currentRatio") or 0
-        if current_ratio < 0.5:
-            # ratio below 0.5
-            continue
+            if not ticker_info.get("sector"):
+                # not categorized
+                continue
 
-        end = date.today()
-        start = end - timedelta(days=14)
+            current_ratio = ticker_info.get("currentRatio") or 0
+            if current_ratio < 0.5:
+                # ratio below 0.5
+                continue
 
-        history = yf.download(symbol, start=start, end=end, interval="1d")
-        if len(history) == 0:
-            # not being traded anymore, no trades in last 14 hours
-            continue
+            end = date.today()
+            start = end - timedelta(days=14)
 
-        revived.append(symbol)
+            history = yf.download(
+                symbol, start=start, end=end, interval="1d", progress=False
+            )
+            if len(history) == 0:
+                # not being traded anymore, no trades in last 14 hours
+                continue
 
-    return revived
+            revived.append(symbol)
+
+        return revived
 
 
 if __name__ == "__main__":
