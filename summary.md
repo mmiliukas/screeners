7b5c24f009061f02c4f0a3e2734bcb4aa384ab6e
Mantas Miliukas <mmiliukas@gmail.com>
2024-10-04 21:45:27 +0300

    .

---
+++ summary.md
@@ -0,0 +1,15 @@
+```csv
+                tickers  12.67 MB 2635
+                   runs   9.35 MB 5032
+             first-seen   2.98 MB 2999
+                reports  95.30 KB   10
+              screeners  47.92 KB   25
+                actions  40.16 KB   20
+                   etfs   4.30 KB   13
+            tickers.csv 807.80 KB    1
+tickers-giga-10-100.csv 594.45 KB    1
+   tickers-giga-1-5.csv 253.02 KB    1
+  tickers-giga-5-10.csv 164.07 KB    1
+    tickers-ignored.csv  47.32 KB    1
+       tickers-etfs.csv  28.88 KB    1
+```
---
+++ summary.py
@@ -0,0 +1,50 @@
+import math
+import os
+
+import pandas as pd
+
+
+def convert_size(size_bytes) -> str:
+    if size_bytes == 0:
+        return "0 B"
+    size_name = ("B", "KB", "MB", "GB", "TB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = size_bytes / p
+    return f"{s:.2f} {size_name[i]}"
+
+
+def summarize_file(file: str) -> tuple[str, int, str, int]:
+    size = os.path.getsize(file)
+    return (file, size, convert_size(size), 1)
+
+
+def summarize_dir(dir: str) -> tuple[str, int, str, int]:
+    size = 0
+    count = 0
+
+    for dirpath, dirnames, filenames in os.walk(dir):
+        for file in filenames:
+            size += os.path.getsize(os.path.join(dirpath, file))
+            count += 1
+
+    return dir, size, convert_size(size), count
+
+
+if __name__ == "__main__":
+    entries = os.listdir(".")
+
+    files = [file for file in entries if os.path.isfile(file) and file.endswith(".csv")]
+    dirs = [dir for dir in entries if os.path.isdir(dir) and not dir.startswith(".")]
+
+    df_dirs = pd.DataFrame([summarize_dir(dir) for dir in dirs])
+    df_files = pd.DataFrame([summarize_file(file) for file in files])
+
+    df_dirs.sort_values(by=1, inplace=True, ascending=False)  # type: ignore
+    df_files.sort_values(by=1, inplace=True, ascending=False)  # type: ignore
+
+    df = pd.concat([df_dirs, df_files])
+
+    print("```csv")
+    print(df[[0, 2, 3]].to_string(index=False, header=False))
+    print("```")
