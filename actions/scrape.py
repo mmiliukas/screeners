e2b3882028cfbf80e0fdf130a383c7ab54376390
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-15 21:48:36 +0200

    .

--- actions/scrape.py
+++ actions/scrape.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from playwright.sync_api import sync_playwright
 
 from screeners.config import config
@@ -9,10 +11,13 @@ def scrape(cookies: str, cache_name: str, url: str) -> None:
 
     retry_times = config["scraper"]["retry_times"]
 
-    with sync_playwright() as playwright:
-        page = open_page(playwright, cookies)
+    try:
+        with sync_playwright() as playwright:
+            page = open_page(playwright, cookies)
 
-        file_name = unique_file_name(extension=".csv")
-        target = abs_path(cache_name, file_name)
+            file_name = unique_file_name(extension=".csv")
+            target = abs_path(cache_name, file_name)
 
-        retry(retry_times)(lambda: scrape_screener(page, url, target))
+            retry(retry_times)(lambda: scrape_screener(page, url, target))
+    except asyncio.exceptions.CancelledError:
+        pass
