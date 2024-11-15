54c7075e3a673a39163ab9c4083e7eecdb4cb66b
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-15 21:03:58 +0200

    .

--- actions/scrape.py
+++ actions/scrape.py
@@ -5,15 +5,14 @@ from screeners.scraper import open_page, scrape_screener
 from screeners.utils import abs_path, retry, unique_file_name
 
 
-def scrape(cookies: str) -> None:
+def scrape(cookies: str, cache_name: str, url: str) -> None:
 
     retry_times = config["scraper"]["retry_times"]
 
     with sync_playwright() as playwright:
         page = open_page(playwright, cookies)
 
-        for screener in config["screeners"]:
-            file_name = unique_file_name(extension=".csv")
-            target = abs_path(screener["cache_name"], file_name)
+        file_name = unique_file_name(extension=".csv")
+        target = abs_path(cache_name, file_name)
 
-            retry(retry_times)(lambda: scrape_screener(page, screener["url"], target))
+        retry(retry_times)(lambda: scrape_screener(page, url, target))
