a1f93ecf574c6e3d71b914575c5d0d3f268a68d3
Mantas Miliukas <mmiliukas@gmail.com>
2024-11-29 22:49:10 +0200

    .

--- actions/giga.py
+++ actions/giga.py
@@ -8,12 +8,9 @@ from screeners.utils import abs_path, retry
 def giga(cookies: str, index: int) -> None:
 
     retry_times = config["scraper"]["retry_times"]
+    giga = config["giga"][index]
+    to_file_name = abs_path(giga["target"])
 
     with sync_playwright() as playwright:
         page = open_page(playwright, cookies)
-
-        screener = config["giga"][index]
-        url = screener["url"]
-        target = abs_path(screener["target"])
-
-        retry(retry_times)(lambda: scrape_screener(page, url, target))
+        retry(retry_times)(lambda: scrape_screener(page, giga["url"], to_file_name))
