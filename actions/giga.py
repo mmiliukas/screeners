34be1851760df664ecd5bd02c68e9b1f3d4a8f40
Mantas Miliukas <mmiliukas@gmail.com>
2024-12-06 22:13:15 +0200

    .

--- actions/giga.py
+++ actions/giga.py
@@ -1,14 +1,20 @@
+import logging
+
 from playwright.sync_api import sync_playwright
 
 from screeners.config import config
 from screeners.scraper import open_page, scrape_screener
-from screeners.utils import abs_path, retry
+from screeners.utils import retry
+
+logger = logging.getLogger(__name__)
 
 
 def giga(cookies: str, index: int) -> None:
 
     giga = config.giga[index]
     retry_times = config.scraper.retry_times
+
+    logger.info(f"scraping {giga.name}...")
 
     with sync_playwright() as playwright:
         page = open_page(playwright, cookies)
