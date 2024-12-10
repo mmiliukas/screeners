f00a32fd16b0f62726d494fb36486ed1878e8fdc
Mantas Miliukas <mmiliukas@gmail.com>
2024-12-10 23:23:13 +0200

    .

--- actions/cookies.py
+++ actions/cookies.py
@@ -1,9 +1,27 @@
 import base64
 import json
+import re
+from time import sleep
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, sync_playwright
 
-from screeners.scraper import login
+
+def login(page: Page, username: str, password: str) -> None:
+    login_url = "https://login.yahoo.com"
+
+    page.goto(login_url)
+    page.wait_for_selector("input#login-username")
+    page.type("input#login-username", username, delay=100)
+    page.click("input#login-signin")
+    page.wait_for_selector("input#login-passwd")
+    page.type("input#login-passwd", password, delay=120)
+
+    sleep(2)
+
+    page.click("button#login-signin")
+
+    url_to_wait = re.compile(".*www.yahoo.com.*", re.IGNORECASE)
+    page.wait_for_url(url_to_wait, wait_until="domcontentloaded")
 
 
 def cookies(username: str, password: str) -> str:
--- screeners/scraper/__init__.py
+++ screeners/scraper/__init__.py
@@ -1,4 +1,3 @@
-from .login import login
 from .open_page import open_page
 from .scrape_etf import scrape_etf
 from .scrape_screener import scrape_screener
