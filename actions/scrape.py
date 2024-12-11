b644ca3bccf0aa8c8b62bab2cfdd2d675758ec72
Mantas Miliukas <mmiliukas@gmail.com>
2024-12-11 22:03:06 +0200

    .

--- actions/scrape.py
+++ actions/scrape.py
@@ -1,11 +1,15 @@
+import logging
+
 from playwright.sync_api import sync_playwright
 
 from screeners.config import config
 from screeners.scraper import open_page, scrape_screener
 from screeners.utils import abs_path, retry, unique_file_name
 
+logger = logging.getLogger(__name__)
 
-def scrape(cookies: str, cache_name: str, url: str) -> None:
+
+def scrape(name: str, cookies: str, cache_name: str, url: str) -> None:
 
     retry_times = config.scraper.retry_times
 
@@ -15,4 +19,5 @@ def scrape(cookies: str, cache_name: str, url: str) -> None:
         file_name = unique_file_name(extension=".csv")
         target = abs_path(cache_name, file_name)
 
+        logger.info(f"scraping {name}")
         retry(retry_times)(lambda: scrape_screener(page, url, target))
