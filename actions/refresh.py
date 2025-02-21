12cfe27778dcef70f87371ea80610345f7ac3a81
Mantas Miliukas <mmiliukas@gmail.com>
2025-02-21 22:48:37 +0200

    .

--- actions/refresh.py
+++ actions/refresh.py
@@ -16,7 +16,10 @@ logger = logging.getLogger(__name__)
 
 def last_fetched_in_days(path: str) -> int:
     with open(path, "r") as file:
-        fetch_time = json.load(file)[0].get("__fetch_time")
+        try:
+            fetch_time = json.load(file)[0].get("__fetch_time")
+        except Exception:
+            fetch_time = "2024-01-01"
     fetch_time = datetime.date.fromisoformat(fetch_time)
     today = datetime.date.today()
     return (today - fetch_time).days
