80fc9e232a9b75eb777507b7f313d69772ae9585
Mantas Miliukas <mmiliukas@gmail.com>
2025-08-05 20:38:15 +0300

    chore: wip

--- screeners/scraper/scrape_screener.py
+++ screeners/scraper/scrape_screener.py
@@ -84,7 +84,7 @@ def scrape_screener_single(page: Page, url: str, date: str) -> pd.DataFrame:
         raise Exception(f"screener {url} not found, might be session has expired")
 
     try:
-        page.wait_for_timeout(1_000)
+        page.wait_for_timeout(2_000)
         page.wait_for_selector(
             f"{selector_table} > table > thead > tr", state="visible", strict=True
         )
