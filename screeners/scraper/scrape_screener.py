22474e294272a29723ec6e7b317672d5d6f9df46
Mantas Miliukas <mmiliukas@gmail.com>
2025-08-05 18:28:12 +0300

    chore: wip

--- screeners/scraper/scrape_screener.py
+++ screeners/scraper/scrape_screener.py
@@ -58,7 +58,7 @@ def scrape_screener(page: Page, url: str, target: str) -> None:
 
     while True:
         url_to_scape = url if offset == 0 else f"{url}&start={offset}"
-        logger.info(f"scraping {offset:>4} from {url} to {target}...")
+        logger.info(f"scraping {offset:>4} from {url_to_scape} to {target}...")
 
         df = scrape_screener_single(page, url_to_scape, date)
 
